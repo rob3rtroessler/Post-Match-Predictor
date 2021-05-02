2c33f0216ec5f70f96e48074b6d806ad8b7ecf16
unknown <robertroessler@g.harvard.edu>
2021-05-02 17:06:36 -0400

    bug fixes

--- application.py
+++ application.py
@@ -74,5 +74,6 @@ def send_corpus_json():
 
     return data
 
-if __name__ == '__main__': # Run flask when the file is called
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
