b81a2299cbd48bf9f1c951737d20a460e35f8194
unknown <robertroessler@g.harvard.edu>
2021-05-06 11:18:03 -0400

    fix bug setting reloader to false

--- application.py
+++ application.py
@@ -148,4 +148,4 @@ def send_latest_predictions():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, use_reloader=False)
